676e002093d7a713a054446b7a54eea1db857451
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-27 19:37:10 +0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,12 +1,19 @@
 class Dog implements Animal {
 
     @Override
+    
     public void eat() {
+        
         System.out.println("Dog eats meat.");
+        
     }
 
     @Override
+    
     public void sound() {
+        
         System.out.println("Dog barks.");
+        
     }
+    
 }
