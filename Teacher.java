e039291f54478f61ba4e24c439b2adb2c2aa2833
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-27 19:38:42 +0500

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,12 +1,19 @@
 class Teacher implements Human {
 
     @Override
+    
     public void work() {
+        
         System.out.println("Teacher teaches students.");
+        
     }
 
     @Override
+    
     public void speak() {
+        
         System.out.println("Teacher gives lectures.");
+        
     }
+    
 }
