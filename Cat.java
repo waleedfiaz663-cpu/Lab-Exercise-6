f7ce46a4931ed7e02e45d8fd2759525164a6bfd5
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-27 19:36:31 +0500

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,12 +1,19 @@
 class Cat implements Animal {
 
     @Override
+    
     public void eat() {
+        
         System.out.println("Cat drinks milk.");
+        
     }
 
     @Override
+    
     public void sound() {
+        
         System.out.println("Cat meows.");
+        
     }
+    
 }
